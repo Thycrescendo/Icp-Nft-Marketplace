ef5d8e667af5072c7d3cddf543cf7f20a378c9c3
thycrescendo <dmicholn@gmail.com>
2025-03-24 09:14:03 +0100

    took out the compilers

--- Smart-contracts/scripts/verify.js
+++
@@ -1,9 +0,0 @@
-
-const hre = require("hardhat");
-const { ethers } = require("hardhat");
-const network = hre.network.name;
-
-main().catch((error) => {
-  console.error(error);
-  process.exitCode = 1;
-});
