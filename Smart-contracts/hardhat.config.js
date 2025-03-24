8d7ef4174e22c20d0d2e1b53c4eaa68fc37b40fb
thycrescendo <dmicholn@gmail.com>
2025-03-24 09:02:32 +0100

    interpool chain

--- Smart-contracts/hardhat.config.js
+++ Smart-contracts/hardhat.config.js
@@ -1,24 +1,7 @@
 require("@nomicfoundation/hardhat-toolbox");
 require("@nomicfoundation/hardhat-verify");
 let secrets = require("./secrets");
-
-/** @type import('hardhat/config').HardhatUserConfig */
-module.exports = {
-  solidity: {
-    compilers: [
-      {
-        version: "0.8.20",
-        settings: {
-          optimizer: {
-            enabled: true,
-            runs: 200,
-          },
-        },
-      },
-    ],
-  },
-  networks: {
-    sepolia: {
+polia: {
       url: secrets.url,
       accounts: [secrets.key],
       apiKey: secrets.apiKey,
