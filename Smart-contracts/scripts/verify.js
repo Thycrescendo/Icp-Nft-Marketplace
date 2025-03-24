cc92bf65d2778fa667d163193efd4c974879f7e7
thycrescendo <dmicholn@gmail.com>
2025-03-24 09:10:46 +0100

    took out the compilers

--- Smart-contracts/scripts/verify.js
+++ Smart-contracts/scripts/verify.js
@@ -11,21 +11,6 @@ const hre = require("hardhat");
 const { ethers } = require("hardhat");
 const network = hre.network.name;
 
-async function main() {
-  // for sepolia network
-  const contractAddress = "0x2B5474bCCCae8C9cce8D70Ed0e24B8D3797b2BAD";
-  const router = "0x0BF3dE8c5D3e8A2B34D2BEeB17ABfCeBaf363A59";
-  const linkToken = "0x779877A7B0D9E8603169DdbD7836e478b4624789";
-  const owner = "0x751f24E6FF4A12466a5249Fe380a7cC412dE5164";
-  const interval = 60;
-
-  await hre.run("verify:verify", {
-    address: contractAddress,
-    constructorArguments: [router, linkToken, owner, interval],
-    contract: "contracts/Pool.sol:ZKLiquidAutoPool",
-  });
-}
-
 main().catch((error) => {
   console.error(error);
   process.exitCode = 1;
